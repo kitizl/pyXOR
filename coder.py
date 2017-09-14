a45b093df6ac53f24a5d4b92beed16f9fc407cde
kitizl <nbalasub@ucsc.edu>
2017-09-14 12:44:32 +0530

    Combined the decrpyting and encrypting functions

--- coder.py
+++ coder.py
@@ -4,6 +4,7 @@
 # since XOR operations repeat the results when used repeatedly, we can use
 # it as a method to encrypt-decrypt data.
 
+
 def passkey(word):
     # this function removes all the repeating characters
     # key thing to note - `word` is ALL LOWER CASE
@@ -16,34 +17,20 @@ def passkey(word):
     return password
 
 
-def encry(plaintext,key):
+def crypt(text,key):
+    # this function does both the decoding and encoding
     # key must be in lowercase, or until I update the passkey function
     key = passkey(key.lower())
     
     # repeatin the key string to match with the length of the plaintext
-    key = key*(len(plaintext)//len(key)) + key[:(len(plaintext)%len(key))]
+    key = key*(len(text)//len(key)) + key[:(len(text)%len(key))]
     
-    plain_bytes = list(map(ord,plaintext))
+    text_bytes = list(map(ord,text))
     key_bytes = list(map(ord,key))
 
-    encoded_bytes = [ (p^k) for (p,k) in zip(plain_bytes,key_bytes) ]
+    coded_bytes = [ (t^k) for (t,k) in zip(text_bytes,key_bytes) ]
 
-    return "".join(list(map(chr,encoded_bytes)))
-
-
-def decry(codetext,key):
-    # even though the encry() and decry() functions do the same thing, 
-    # I want to separate them for better clarity
-
-    key = passkey(key.lower())
-    
-    key = key*(len(codetext)//len(key)) + key[:(len(codetext)%len(key))]
-    
-    code_bytes = list(map(ord,codetext))
-    key_bytes = list(map(ord,key))
-
-    decoded_bytes = [ (c^k) for (c,k) in zip(code_bytes,key_bytes) ]
-
-    return "".join(list(map(chr,decoded_bytes))) 
+    return "".join(list(map(chr,coded_bytes)))
 
 # TODO : Frontend stuff
+
